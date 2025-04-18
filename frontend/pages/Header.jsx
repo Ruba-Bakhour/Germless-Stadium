3b24ca3a25bfee904e82e83467b6eaf7dad7404e
Raghad_Harith <raharith@stu.kau.edu.sa>
2025-04-18 18:09:55 +0300

    Fixed the database path of Header.jsx

--- frontend/pages/Header.jsx
+++ frontend/pages/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '../supabaseClient'; // Adjust the path if needed
+import { supabase } from './supabaseClient'; // Adjust the path if needed
 
 const Header = ({ showBackButton = false, title = "Germless Stadium" }) => {
   const router = useRouter();
