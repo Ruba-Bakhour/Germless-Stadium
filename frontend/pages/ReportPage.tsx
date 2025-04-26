a97db2a32fc64ecfcab670b06e1a839cfcab0a3c
Raghad_Harith <raharith@stu.kau.edu.sa>
2025-04-26 16:42:19 +0300

    Deleted two unused buttons in report page

--- frontend/pages/ReportPage.tsx
+++ frontend/pages/ReportPage.tsx
@@ -99,17 +99,13 @@ const ReportsPage = () => {
           </div>
 
           {/* Report Details */}
-          <div className="w-1/3 bg-white p-4 rounded-md shadow-md mt-24">
+          <div className="w-1/3 h-40 bg-white p-4 rounded-md shadow-md mt-24">
             {selectedReport ? (
               <>
                 <h3 className="text-gray-600 text-lg font-bold mb-3">{selectedReport.title}</h3>
                 <p className="text-gray-600 text-sm mb-2"><strong>Date and Time:</strong> {selectedReport.created_at}</p>
                 <p className="text-gray-600 text-sm mb-2"><strong>Total Ready seats:</strong> {selectedReport.total_seats} seats</p>
                 <p className="text-gray-600 text-sm mb-2"><strong>Total Distance:</strong> {selectedReport.distance}</p>
-                <div className="mt-3 flex gap-2">
-                  <button className="text-gray-600 px-4 py-2 bg-white border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200">Analyzed images</button>
-                  <button className="text-gray-600 px-4 py-2 bg-white border border-gray-300 rounded-md cursor-pointer hover:bg-gray-200">Route image</button>
-                </div>
               </>
             ) : (
               <p className="text-gray-600 text-sm">Select a report to view details</p>
